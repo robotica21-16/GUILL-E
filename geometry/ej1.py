a75c9e390df47f1ca87592a70359819dc08b6d88
nestor <nestormg98@gmail.com>
2021-02-25 12:15:08 +0100

    Mi ej 1

---
+++ geometry/ej1.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from plot_robot import *
+from geometry import *
+from simubot import *
+
+def printsep(s, sep="------------"):
+    print(sep, s, sep)
+
+def robotFromPuerta(rxp, wxp):
+    """
+    Devuelve wxr (pos del robot global) a partir de la pos global de la puerta
+    (wxp) y su pos relativa respecto al robot (rxp)
+    """
+    # return wxp-np.linalg.inv(hom(rxp))
+    return loc(np.dot(hom(wxp), np.linalg.inv(hom(rxp))))
+    #return loc(np.dot(hom(rxp),wxp))
+
+
+def pltEscenario():
+    fig1, ax = plt.subplots()
+
+    ax.set_xlim(0, 6)
+    ax.set_ylim(0, 11)
+
+    # frecuencia de los labels:
+    ax.set_xticks(np.arange(0, 6, 1))
+    ax.set_yticks(np.arange(0, 11, 1))
+
+    ax.set_aspect('equal')
+    # ax.set_box_aspect(2)
+    # Cuadricula:
+    plt.grid(True)
+
+def dibPunto(w_x_p):
+    plt.plot(w_x_p[0], w_x_p[1], 'o')
+    #plt.plot(w_x_p[1], w_x_p[0], 'o')
+
+
+if __name__ == '__main__':
+    # ------------------------- 1 -------------------------------
+    printsep('Apartado 1')
+    wxp = np.array([2.5, 10, 0]) # pos global de la puerta
+    rxp = np.array([5.2,-3,norm_pi_deg(-125)]) # pos de la puerta para el robot
+    wxr = robotFromPuerta(rxp, wxp) # pos robot en mundo
+    print('Robot en:', wxr)
+    pltEscenario()
+    # dibrobot(wxr, tamano='g')
+    dibPunto(wxp)
+    # plt.show()
+    # exit(0)
+    # ------------------------- 2 -------------------------------
+    printsep('Apartado 2')
+    rxc = np.array([4.17,0.73,norm_pi_deg(-35)]) # pos del cuadro para el robot
+    wxc = loc(np.dot((hom(wxr)),hom(rxc))) # cuadro en mundo
+    print('Cuadro en:', wxc)
+    dibPunto(wxc)
+    # plt.show()
+    # ------------------------- 3 -------------------------------
+    printsep('Apartado 3')
+    pxc = loc(np.dot(np.linalg.inv(hom(wxp)),hom(wxc))) # cuadro para puerta
+    print('Cuadro para la puerta:', pxc)
+    # ------------------------- 4 -------------------------------
+    printsep('Apartado 4')
+    t = 8 # 8 segundos
+    r, th = radioYThTrayectoriaCirc(rxp) # radio y th
+    # print(r, th)
+    long = longArco(r, th) # longitud del arco = r*th
+    #print(long)
+    v = long / t # velocidad tangencial, distancia/t
+    w = wVelFromVR(v, r) # vel angular, r/v
+    print('Velocidad lineal, angular:', v, ",", w)
+    posrel = trayectoriaCircular(r, th)#
+    print('Localizacion final relativa:', posrel)
+    print('Localizacion final w:', loc(np.dot(hom(wxr), hom(posrel))))
+
+    # ------------------------- 5 -------------------------------
+    printsep('Apartado 5')
+
+    print('A los 4 segundos:')
+    posW, posR = simubot([v,w], wxr, 4)
+    print('\tLocalizacion en W:', posW,
+        '\n\trelativa:         ', posR)
+
+    print('Cada segundo del movimiento:')
+    fps = 4
+    for i in range(fps*(t)+1):
+        posW, posR = simubot([v,w], wxr, i/fps)
+        if (i % fps == 0):
+            print('t =', i/fps, 's:')
+            print('\tLocalizacion en W:', posW,
+                '\n\trelativa:         ', posR)
+
+        pltbot = dibrobot(posW, tamano='g')
+        plt.pause(1/fps)
+        #print(pltbot)
+        if i/fps<t:
+            pltbot.remove()
+
+    plt.show()
---
+++ geometry/geometry.py
@@ -0,0 +1,106 @@
+"""
+Vectors are np.array[x,y,th]
+"""
+
+import numpy as np
+import math
+
+
+
+def mapf(val, mind, maxd, minr, maxr):
+    """
+    Cambia val del dominio [mind,maxd] al rango [minr,maxr]
+    """
+    return (val - mind) * (maxr - minr) / (maxd - mind) + minr
+
+
+def norm_pi_deg(th):
+    """
+    Normaliza el angulo th (en grados) a 0..PI o 0..-PI (-PI..+PI)
+    """
+    # Nota: crrreo que se podria hacer mas eficiente evitando el if/else
+    deg = th%360 # se evitan vueltas de mas
+    if deg <= 180: # mitad superior
+        return mapf(deg, 0, 180, 0, math.pi)
+    else: # inferior
+        return mapf(deg, 180, 360, -math.pi, 0)
+
+
+def norm_pi(th):
+    """
+    Normaliza el angulo th (en grados) a 0..PI o 0..-PI (-PI..+PI)
+    """
+    # Nota: crrreo que se podria hacer mas eficiente evitando el if/else
+    th_out = th%(2.0*math.pi) # se evitan vueltas de mas
+    if th_out <= math.pi: # mitad superior
+        return th_out # mapf(deg, 0, 180, 0, math.pi)
+    else: # inferior
+        return -(2*math.pi-th_out)# mapf(deg, 180, 360, -math.pi, 0)
+
+
+def rot(th):
+    """
+    Devuelve la matriz de rotacion de th
+    """
+    return np.array([[np.cos(th), -np.sin(th), 0],
+                     [np.sin(th),  np.cos(th), 0],
+                     [0, 0, 1]])
+
+def rotDeg(deg):
+    """
+    Devuelve la matriz de rotacion para deg grados
+    """
+    return rot(norm_pi(deg))
+
+def loc(T):
+    """
+    Devuelve x a partir de T
+    """
+    #print(T[0,2], T[1,2])
+    return np.array([T[0,2], T[1,2], math.atan2(T[1,0],T[0,0])]) # TODO: test
+
+# TODO: test
+def hom(x):
+    """
+    In: x np.array(3x1)
+    Devuelve la matriz T (3x3) que transforma el origen en x
+    """
+    th = x[2]
+    return np.array([[np.cos(th), -np.sin(th), x[0]],
+                     [np.sin(th),  np.cos(th), x[1]],
+                     [0, 0, 1]])
+
+
+def trayectoriaCircular(r, th):
+    """
+    Devuelve el punto relativo al inicial tras girar el angulo th con radio r
+    """
+    return np.array([r * np.sin(th), r * (1-np.cos(th)), th])
+
+
+def trayectoriaCircularDeg(r, deg):
+    """
+    Devuelve el punto relativo al inicial tras girar el angulo th (GRADOS) con radio r
+    """
+    return trayectoriaCircular(r, norm_pi(deg))
+
+def radioYThTrayectoriaCirc(xFin):
+    """
+    Devuelve el radio y el angulo th necesarios para llegar de xFin (local) en un arco de th grados
+    """
+    x, y = xFin[0], xFin[1]
+    return ((x**2+y**2)/(2.0*y), # Radio
+    math.atan2(2.0*x*y, x**2-y**2)) # angulo
+
+def wVelFromVR(v, r):
+    """
+    Devuelve la velocidad angular a partir de v y r
+    """
+    return v/r
+
+
+def longArco(r, th):
+    """
+    Devuelve la longitud del arco de angulo th y radio r, r*th
+    """
+    return r*th
