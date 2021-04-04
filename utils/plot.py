6cf66f8ae4413c720a78225e8c906ebe5ad79157
nestor98 <nestor98@github.com>
2021-04-04 12:30:40 +0200

    plot etc

---
+++ utils/plot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plotFile(fileName, sep="\t"):
+    df  = pd.read_csv(fileName, sep=sep)
+    print(df)
+    #df.plot()  # plots all columns against index
+    ax = df.plot(kind='scatter',x='x',y='y',
+        xlim=[-1.5,0.2], ylim=[-0.3,0.3], title="Odometría - video 1",
+        grid=True) # scatter plot
+    ax.set_xlabel("x (m)")
+    ax.set_ylabel("y (m)")
+    ax.set_aspect('equal')
+    #df.plot(kind='density')  # estimate density function
+    plt.show()
--- utils/utils.py
+++ utils/utils.py
@@ -7,22 +7,6 @@ import math
 import sys
 
 
-# import pandas as pd
-import matplotlib.pyplot as plt
-
-""" def plotFile(fileName, sep="\t"):
-    df  = pd.read_csv(fileName, sep=sep)
-    print(df)
-    #df.plot()  # plots all columns against index
-    ax = df.plot(kind='scatter',x='x',y='y',
-        xlim=[-1.5,0.2], ylim=[-0.3,0.3], title="Odometría - video 1",
-        grid=True) # scatter plot
-    ax.set_xlabel("x (m)")
-    ax.set_ylabel("y (m)")
-    ax.set_aspect('equal')
-    #df.plot(kind='density')  # estimate density function
-    plt.show() """
-
 def writeLog(f_log, data, sep="\t"):
     """
     Writes a row of the log (t, x, y, th, v, w, deltaTh, deltaSi)
