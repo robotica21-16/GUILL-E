5935d7a9d4c04397cc851c3c47cf2f5403dff0d4
Cristina <oriol.cristina.25@gmail.com>
2021-04-24 12:09:24 +0200

    gyro

---
+++ pruebaGyro.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import argparse
+import os
+import numpy as np
+import time
+import traceback
+
+import matplotlib
+matplotlib.use("TkAgg")
+# sudo apt-get install tcl-dev tk-dev python-tk python3-tk if TkAgg is not available
+
+from Robot import Robot
+from p4.MapLib import Map2D
+import math
+
+from trayectorias.trayectorias import *
+
+baldosa=0.4
+
+def mapA(robot):
+    t = TrayectoriaTrabajoA(baldosa-0.02)
+    mapa = Map2D("trabajo/mapaA_CARRERA.txt")
+    return t, mapa
+
+def mapB(robot):
+
+    t = TrayectoriaTrabajoB(baldosa)
+    mapa = Map2D("trabajo/mapaA_CARRERA.txt")
+    return t, mapa
+
+def main(args):
+    """
+    """
+    try:
+        robot = Robot()
+        while(True):
+            print(robot.angleGiro())
+
+
+    except KeyboardInterrupt:
+    # except the program gets interrupted by Ctrl+C on the keyboard.
+    # THIS IS IMPORTANT if we want that motors STOP when we Ctrl+C ...
+        robot.stopOdometry()
+    except BaseException as e:
+        print(e)
+        traceback.print_exc()
+        robot.stopOdometry()
+
+
+
+if __name__ == "__main__":
+
+    # get and parse arguments passed to main
+    # Add as many args as you need ...
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+    main(args)
